f6903a1f695da38fd9783bfe1cd472d93cc6033f
Nagelli pavan kumar <nagelli186@gmail.com>
2026-04-10 16:29:13 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,8 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 
+# New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
