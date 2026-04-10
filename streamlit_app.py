0aa6f60cb0d6cbb544ce416ba7f54a8e9b848542
Nagelli pavan kumar <nagelli186@gmail.com>
2026-04-10 16:46:00 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,14 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # session = get_active_session()
 
 # ✅ Fetch data from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options") \
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')) \
     .select(col('FRUIT_NAME')) \
     .to_pandas()
+
+pd_df = my_dataframe.to_pandas()
+search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
 
 # ✅ Show data
 st.dataframe(data=my_dataframe, use_container_width=True)
